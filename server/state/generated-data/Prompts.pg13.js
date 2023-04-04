02b99d47876d3f16e24564e1f00271e93d944562
Kathryn Tsai <kathryntsai@users.noreply.github.com>
2023-04-04 12:56:11 -0400

    Update prompts

--- server/state/generated-data/Prompts.pg13.js
+++ server/state/generated-data/Prompts.pg13.js
@@ -3,18 +3,29 @@ export function getRandomPG13Prompt() {
 }
 
 const prompts = [
-  "Tristan\u2019s favorite drink is ___",
+  "Tristan\u2019s favorite drink is ____",
   "You have 24 hours to prepare for a fight against Tristan.  What do you do and what do you bring?",
   "Define the axes on which Tristan rates his food.",
   "Tristan\u2019s dog is named Tiger. What would Tristan name his car?",
   "What is Tristan\u2019s go-to workout song?",
-  "What does Tristan say his favorite movie is?  What is his actual favorite movie?",
+  "What does Tristan say his favorite movie is?",
   "Tristan has lost his water bottle again.  Where did he leave it this time?",
-  "Tristan’s childhood email was penguinpuppy0@gmail.com.  What animal is he most similar to?",
-  "Tristan’s undergrad major was chemical engineering. Name Tristan\u2019s favorite element on the periodic table.",
-  "If Tristan weren\u2019t a doctor, what else would he be doing with his life?",
-  "Insert Tristan into a TV series. What would it be?",
-  "What would Tristan's superhero name be?",
-  "If he could choose, what would Tristan's superpower be?", 
-  /* something Houston-related? */
+  "Tristan\u2019s childhood email was penguinpuppy0@gmail.com. To what animal is he most similar?",
+  "Tristan\u2019s undergrad major was chemical engineering. If Tristan\u2019s discovered a new periodic table element, he would name it ___.",
+  "If Tristan weren\u2019t a doctor, he would be a ____.",
+  "Insert Tristan into a TV series. What kind of TV show would it be?",
+  "What would Tristan\u2019s superhero name be?",
+  "If he could choose, what would Tristan\u2019s superpower be?", 
+  "2 minutes left to BeReal!  What can you expect to see in Tristan\u2019s BeReal?",
+  "Tristan\u2019s lab chore should be ___ instead of making PBS?",
+  "If all crime were legal for 24 hours, Tristan would probably ____?",
+  "A food that Tristan absolutely will not eat is ____.",
+  "Tristan can\u2019t find his name tag again! Where did Rob put it?",
+  "Tristan\u2019s hidden talent is ____.",
+  "Tristan\u2019s personality includes a unique *FLASH* of ____.",
+  "Something Tristan got a trophy or award for as a kid was ____.",
+  "If Tristan had an extra room in his apartment, he\u2019d probably use it for ____.",
+  "If Tristan had a sandwich named after him, the two main ingredients would be ____.",
+  "If Tristan could know one piece of information in the universe, it would be ____.",
+  /* something Houston-related? Tristan is from Houston, and the Houston Rockets beat the Phillies in the World Series this year. /
 ];
